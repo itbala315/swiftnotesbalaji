ff6cb6360aee4c268464524220503c0119890784
Ramkumar, Balaji <Balaji.Ramkumar@united.com>
2023-03-17 13:43:43 -0500

    test case fix

--- src/app/shared/directives/click-outside.directive.spec.ts
+++ src/app/shared/directives/click-outside.directive.spec.ts
@@ -1,8 +1,47 @@
 import { ClickOutsideDirective } from './click-outside.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+class TestComponent {
+  clickOutside(): void{}
+}
 
 describe('ClickOutsideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickOutsideDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+
+
+  beforeEach(async () => {
+      await TestBed.configureTestingModule({
+          declarations: [ClickOutsideDirective, TestComponent],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+  });
+
+  it('should create directive', () => {
+      fixture.detectChanges();
+
+      expect(component).toBeTruthy();
+  });
+
+  it('should trigger click inside', () => {
+      spyOn(component, 'clickOutside');    
+      fixture.detectChanges();
+
+      const inside = fixture.debugElement.query(By.css('.inside')).nativeElement;
+      inside.click();
+
+      expect(component.clickOutside).not.toHaveBeenCalled();
+  });
+
+  it('should trigger click outside', () => {
+      spyOn(component, 'clickOutside');
+      fixture.detectChanges();
+
+      const outside = fixture.debugElement.query(By.css('.outside')).nativeElement;
+      outside.click();
+
+      expect(component.clickOutside).toHaveBeenCalled();
   });
 });
